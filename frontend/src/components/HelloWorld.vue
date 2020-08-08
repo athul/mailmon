aed515cbc5b5a79910857cea62bd58e908220bee
athul <athul8720@gmail.com>
2020-08-08 21:25:23 +0530

    Better MD renders and Tabs

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,19 +1,20 @@
 <template>
 <div>
   <h1>MailMon</h1>
-  <label class="label">Email: </label>
-  <input type="email" v-model="email"><br/><br/>
-  <textarea v-model="message" placeholder="Add Email Content in Markdwon" ></textarea>
+  <label>Email Content</label><br/>
+  <textarea v-model="message" placeholder="Add Email Content in Markdwon" ></textarea><br/>
   <button v-on:click="rendermd">Preview MD</button><br/>
-  <div v-html="markdown"></div>
+
+<div v-html="markdown"></div>
   <p>{{email}}</p>
+  <label> Send Emails to </label>
   <select v-model="selected">
   <option v-for="option in options" v-bind:key="option.value" >
     {{ option.text }}
   </option>
 </select>
+<br/>
 <button v-on:click="fetchemails">Click</button>
-<span>Selected: {{ selected }}</span>
 <div v-for="email in emails" :key="email">
 <p>{{email}}</p>
 </div>
@@ -69,7 +70,7 @@ export default {
 <style>
 textarea {
   width: 50%;
-  height: 150px;
+  height: 200px;
   padding: 12px 20px;
   box-sizing: border-box;
   border: 2px solid #ccc;
